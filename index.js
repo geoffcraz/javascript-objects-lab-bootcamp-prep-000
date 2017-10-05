0997cba54bf8707e234629a60990e4beaddff775
geoffcraz <geoffcraz@gmail.com>
2017-10-05 19:22:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,10 @@ return copy
 }
 */
 
+
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
 }
-
-
 
 function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
