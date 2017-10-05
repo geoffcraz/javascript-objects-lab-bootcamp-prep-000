fdccff0bbdbf9bcb90df62183f05d04b10c8d292
geoffcraz <geoffcraz@gmail.com>
2017-10-05 18:44:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ return copy
 
 }
 */
-//function updateObjectWithKeyAndValue(object, key, value) {
+/*
+function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, {[key]: value })
 }
+*/
 
 /*function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
   object[key] = value
